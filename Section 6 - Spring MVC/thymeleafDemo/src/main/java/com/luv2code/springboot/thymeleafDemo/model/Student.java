0c0e27491b66f5b71172bcecb8ef0fb9761b4139
Gabix123 <gabriel.paic@fer.hr>
2025-11-12 12:09:44 +0100

    section-6

---
+++ Section 6 - Spring MVC/thymeleafDemo/src/main/java/com/luv2code/springboot/thymeleafDemo/model/Student.java
@@ -0,0 +1,51 @@
+package com.luv2code.springboot.thymeleafDemo.model;
+
+import java.util.List;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private String country;
+    private String favoriteLanguage;
+    private List<String> favoriteSystems;
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCountry() {
+        return this.country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getFavoriteLanguage() {
+        return this.favoriteLanguage;
+    }
+
+    public void setFavoriteLanguage(String favoriteLanguage) {
+        this.favoriteLanguage = favoriteLanguage;
+    }
+
+    public List<String> getFavoriteSystems() {
+        return this.favoriteSystems;
+    }
+
+    public void setFavoriteSystems(List<String> favoriteSystems) {
+        this.favoriteSystems = favoriteSystems;
+    }
+}
